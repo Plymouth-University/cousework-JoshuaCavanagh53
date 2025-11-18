6081298a7eb66138852af846ab868516e288627b
Joshua Cavanagh <joshua.cavanagh@students.plymouth.ac.uk>
2025-11-18 15:37:05 +0000

    Update Main.js
    Committing so I can do further work on my laptop

--- GetFit/frontend/src/Main.js
+++ GetFit/frontend/src/Main.js
@@ -7,7 +7,10 @@ import {
 
 const MainPage = () => {
   
-  return (
+  const [overview, selectOverview] = useState(true);
+  const [account, selectAccount] = useState(false);
+
+  if (overview) return (
     
     // Overall Container
     <Box
@@ -56,9 +59,9 @@ const MainPage = () => {
             marginTop: 1,
             width: '100%',
             height: '55px',
-            '&:hover': {
-                background: 'linear-gradient(to right, #252525ff 98%, #FF6F00 98%)'
-            },
+            
+            background: 'linear-gradient(to right, #252525ff 98%, #FF6F00 98%)',
+            
             textAlign: 'center',
             display: 'flex',
             alignItems: 'center',
@@ -74,6 +77,8 @@ const MainPage = () => {
             >
               Overview
             </Typography>
+
+          
         </Box>
 
         <Box
@@ -100,6 +105,86 @@ const MainPage = () => {
               Account
             </Typography>
         </Box>
+        
+        <Box
+          
+          sx={{
+            marginTop: 1,
+            width: '100%',
+            height: '55px',
+            '&:hover': {
+                background: 'linear-gradient(to right, #252525ff 98%, #FF6F00 98%)'
+            },
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',}}>
+            
+            <Typography
+              sx={{
+                fontSize: '14px',
+                fontFamily: 'Arial, sans-serif',
+                textAlign: 'center',
+                color: 'white'
+              }}
+            >
+              Calendar  
+            </Typography>
+        </Box>
+
+        <Box
+          onClick={() => {
+            selectOverview(false);
+            selectAccount(true);
+          }}
+          sx={{
+            marginTop: 1,
+            width: '100%',
+            height: '55px',
+            '&:hover': {
+                background: 'linear-gradient(to right, #252525ff 98%, #FF6F00 98%)'
+            },
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',}}>
+            
+            <Typography
+              sx={{
+                fontSize: '14px',
+                fontFamily: 'Arial, sans-serif',
+                textAlign: 'center',
+                color: 'white'
+              }}
+            >
+              Progress
+            </Typography>
+        </Box>
+        
+        <Box
+          sx={{
+            marginTop: "auto",
+            width: '100%',
+            height: '55px',
+            '&:hover': {
+                background: 'linear-gradient(to right, #252525ff 98%, #FF6F00 98%)'
+            },
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',}}>
+            
+            <Typography
+              sx={{
+                fontSize: '14px',
+                fontFamily: 'Arial, sans-serif',
+                textAlign: 'center',
+                color: 'white'
+              }}
+            >
+              Log out
+            </Typography>
+        </Box>
       </Box>
 
       {/* Main Content Area */}
@@ -123,77 +208,252 @@ const MainPage = () => {
         >
           Welcome back, User!
         </Typography>
-        <Box 
+        <Box
           sx={{
-            padding: 4,
-            width: '20%',
-            height: '20%',
-            background: '#FF6F00',
-            margin: 4,
-            borderRadius: 4,
             display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            
+            flexDirection: 'row'      
+          }}
+        >
+          <Box 
+            sx={{
+              padding: 4,
+              minWidth: '20%',
+              minHeight: 'auto',
+              background: '#FF6F00',
+              margin: 4,
+              borderRadius: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
 
-          }}>
-            <Typography
-                sx={{ 
-                    color: 'white',
-                    fontSize: '20px',
-                    fontWeight: 'bold',
-                    fontFamily: 'Arial, sans-serif',
-                    textAlign: 'center'
 
-                }}
-                >
-                    Current Weight
-                </Typography>
-            
-            <Typography
-                sx={{ 
-                    color: 'white',
-                    fontSize: '50px',
-                    fontFamily: 'Arial, sans-serif',
-                    textAlign: 'center',
-                    fontWeight: 'bold',
-                    marginTop: 2
-                    }}>
-                        75 kg
-                    </Typography>
+            }}>
+              <Typography
+                  sx={{ 
+                      color: 'white',
+                      fontSize: '20px',
+                      fontWeight: 'bold',
+                      fontFamily: 'Arial, sans-serif',
+                      textAlign: 'center'
 
-        </Box>
+                  }}
+                  >
+                      Current Weight
+                  </Typography>
+                
+              <Typography
+                  sx={{ 
+                      color: 'white',
+                      fontSize: '50px',
+                      fontFamily: 'Arial, sans-serif',
+                      textAlign: 'center',
+                      fontWeight: 'bold',
+                      marginTop: 2
+                      }}>
+                          75 kg
+                      </Typography>
 
-        <Box 
-          sx={{
-            padding: 4,
-            width: '20%',
-            height: '20%',
-            background: '#FF6F00',
-            margin: 4,
-            borderRadius: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            
+          </Box>
 
-          }}>
-            <Typography
-                sx={{ 
-                    color: 'white',
-                    fontSize: '20px',
-                    fontWeight: 'bold',
-                    fontFamily: 'Arial, sans-serif',
-                    textAlign: 'center'
+          <Box 
+            sx={{
+              padding: 4,
+              width: '20%',
+              height: 'auto',
+              background: '#FF6F00',
+              margin: 4,
+              borderRadius: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
 
-                }}
-                >
-                    Current Weight
-                </Typography>
+
+            }}>
+              <Typography
+                  sx={{ 
+                      color: 'white',
+                      fontSize: '20px',
+                      fontWeight: 'bold',
+                      fontFamily: 'Arial, sans-serif',
+                      textAlign: 'center'
+
+                  }}
+                  >
+                    Total workouts this month
+                  </Typography>
+              
+              <Typography
+                  sx={{ 
+                      color: 'white',
+                      fontSize: '50px',
+                      fontFamily: 'Arial, sans-serif',
+                      textAlign: 'center',
+                      fontWeight: 'bold',
+                      marginTop: 2
+                      }}>
+                          18 visits
+                      </Typography>
+          </Box>
         </Box>
       </Box>
     </Box>
   );
+
+  if (account) return (
+     // Overall Container
+    <Box
+      display="flex"
+      minHeight="100vh"
+      sx={{
+        bgcolor: '#1E1E1C', 
+      }}
+    >
+      {/* Sidebar */}
+      <Box
+        sx={{
+          width: '18%',
+          minHeight: '100vh',
+          bgcolor: '#2A2A2A', 
+          padding: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',     // Horizontally center content
+        }}
+      >
+        {/* Logo + Text */}
+        <Box
+          display="flex"
+          alignItems="end"
+          textAlign="end"
+          gap={2}
+          margin={2}
+        >
+          
+          <Typography
+            sx={{
+              color: '#f9f9f9ff',
+              fontWeight: 'bold',
+              fontSize: '25px',
+              letterSpacing: '1px',
+              fontFamily: 'Arial, sans-serif'
+            }}
+          >
+            GetFit
+          </Typography>
+        </Box>
+
+        <Box
+          onClick={() => {
+            selectOverview(true);
+            selectAccount(false);
+          }}
+          sx={{
+            marginTop: 1,
+            width: '100%',
+            height: '55px',
+            ":hover": {
+                background: 'linear-gradient(to right, #252525ff 98%, #FF6F00 98%)'
+            },
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',}}>
+            
+            <Typography
+              sx={{
+                fontSize: '14px',
+                fontFamily: 'Arial, sans-serif',
+                textAlign: 'center',
+                color: 'white'
+              }}
+            >
+              Overview
+            </Typography>
+
+          
+        </Box>
+
+        <Box
+          onClick={() => {
+            selectOverview(false);
+            selectAccount(true);
+          }}
+          sx={{
+            marginTop: 1,
+            width: '100%',
+            height: '55px',
+          
+            background: 'linear-gradient(to right, #252525ff 98%, #FF6F00 98%)',
+            
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',}}>
+            
+            <Typography
+              sx={{
+                fontSize: '14px',
+                fontFamily: 'Arial, sans-serif',
+                textAlign: 'center',
+                color: 'white'
+              }}
+            >
+              Account
+            </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            marginTop: "auto",
+            width: '100%',
+            height: '55px',
+            '&:hover': {
+                background: 'linear-gradient(to right, #252525ff 98%, #FF6F00 98%)'
+            },
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',}}>
+            
+            <Typography
+              sx={{
+                fontSize: '14px',
+                fontFamily: 'Arial, sans-serif',
+                textAlign: 'center',
+                color: 'white'
+              }}
+            >
+              Log out
+            </Typography>
+        </Box>
+      </Box>
+
+      {/* Main Content Area */}
+      <Box
+        sx={{
+          flexGrow: 1,
+          minHeight: '100vh',
+          bgcolor: '#1E1E1C',
+        }}
+      >
+        {/* Main content goes here */}
+
+        <Typography
+          sx={{
+            color: 'white',
+            margin: 4,
+            fontSize: '20px',
+            fontWeight: 'bold',
+            fontFamily: 'Arial, sans-serif'
+          }}
+        >
+          Welcome back, User!
+        </Typography>
+        
+      </Box>
+    </Box>
+  );
+
 };
 
 export default MainPage;
